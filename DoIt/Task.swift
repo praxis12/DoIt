2c65f2d8e6c83188cc3089cecb3efbb4e533be0e
Josh Arntz <jarntz12@gmail.com>
2017-04-04 13:54:16 -0500

    Added Task class

---
+++ DoIt/Task.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  DoIt
+//
+//  Created by Josh Arntz on 4/4/17.
+//  Copyright © 2017 Mr. Bear. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var name = ""
+    var important = false
+    
+}
